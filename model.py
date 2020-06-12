418380bbad335c04ff02e2f7aa7d60724d3a8a10
Aris Zhu <ariscjj@outlook.com>
2020-06-11 21:25:29 -0700

    Created Save As Box, Burned Boc, and checkbox list. Attempted to create Toggle Switch

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 # This is the backend (does the math)
-import tkinter
+from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
 root = Tk()
-root.geometry('700x400')
+root.geometry('1000x400')
 #Create Tab Control
 TAB_CONTROL = ttk.Notebook(root)
 #Tab1- Control
@@ -17,15 +18,84 @@ TAB_CONTROL.pack(expand=1, fill="both")
 
 
 #Transaction Setting
-Label(TAB1, text="Transaction Setting").grid(row=0, sticky=W)
+Label(TAB1, text="Transaction Setting").grid(row=0, columnspan=6, sticky=W)
 Label(TAB1, text="Address (Dec)").grid(row=1, sticky=E)
 Label(TAB1, text="Burst Length (Dec)").grid(row=2, sticky=E)
 Label(TAB1, text="Write Data (Hex)").grid(row=3, sticky=E)
-Entry(TAB1).grid(row=1,column=1)
-Entry(TAB1).grid(row=2,column=1)
-Entry(TAB1).grid(row=3,column=1)
-Button(TAB1, text="random").grid(row=2,column=2)
-Button(TAB1, text="random").grid(row=3,column=2)
+Entry(TAB1).grid(row=1,column=1,columnspan=2)
+Entry(TAB1).grid(row=2,column=1,columnspan=2)
+Entry(TAB1).grid(row=3,column=1,columnspan=2)
+Button(TAB1, text="random").grid(row=2,column=3)
+Button(TAB1, text="random").grid(row=3,column=3)
+
+#Save As Box
+Label(TAB1, text="Save As").grid(row=0, column=4,columnspan=2,padx = 20,sticky=W)
+Entry(TAB1).grid(row=1,column=4,padx = 20,columnspan=1, sticky=E)
+Label(TAB1, text=".txt").grid(row=1, column=4,sticky=E)
+Button(TAB1, text="Save").grid(row=2,column=4,sticky=N+E+S+W,padx = 60)
+
+#Burned?
+Label(TAB1, text="Burned?").grid(row=0, column=5,columnspan=1, sticky=E)
+Entry(TAB1).grid(row=0,column=6,columnspan=1, sticky=W)
+Label(TAB1, text=".txt").grid(row=1, column=4,sticky=E)
+
+Label(TAB1, text="Click Freq (Khx)").grid(row=1, column=5,columnspan=2, sticky=S)
+Entry(TAB1).grid(row=2,column=5,columnspan=2, padx=80)
+
+Label(TAB1, text="Voltage (V)").grid(row=3, column=5,columnspan=2)
+Entry(TAB1).grid(row=4,column=5,columnspan=2, padx=80, sticky=N)
+
+
+#Row of checkbuttons
+Checkbutton(TAB1, text="CENG_MODE").grid(row=6,column=6,columnspan=2, sticky=W)
+Checkbutton(TAB1, text="BURN_MODE").grid(row=7,column=6,columnspan=2, sticky=W)
+Checkbutton(TAB1, text= "IENG_MODE").grid(row=8,column=6,columnspan=2, sticky=W)
+Checkbutton(TAB1, text= "TX enable").grid(row=9,column=6,columnspan=2, sticky=W)
+'''
+def toggle():
+    if var.get() == "ON":
+        print("turning on...")
+    else:
+        print("turning off...")
+'''
+var = tk.StringVar()
+tk.Checkbutton(TAB1, onvalue="ON", offvalue="OFF", width=4, indicatoron=False,variable=var, textvariable=var,
+selectcolor="green", background="red").grid(row=5,column=4)
+
+
+var.set("OFF")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
